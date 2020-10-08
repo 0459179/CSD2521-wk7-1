64fe4b2abf54242b17c81a487d5f513ea19ace20
0459179 <70179792+0459179@users.noreply.github.com>
2020-10-08 17:49:33 -0400

    Update HelloTest1.java
    I added the date that I updated this document on line 3. 
    I changed the comments on line 8 as well.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,13 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on 10/08/2020 CJB
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first Pull
+     *  Request for this class. 
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
